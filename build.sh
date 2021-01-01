cccbad6312ddeed25cd52e7ee93493cee4f3aca5
Edward <lued2018@gmail.com>
2021-01-01 14:36:19 -0600

    use -s SINGLE_FILE=1 and reorganize code

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# if [ ! -f "opencv/build_wasm/em-flags.txt" ]
+# then
+#     curl --location "https://github.com/webarkit/opencv-em/releases/download/0.0.3/opencv-em-4.5.0-alpha-rc1.zip" -o opencv-em.zip
+#     unzip -o opencv-em.zip -d opencv
+#     cp -avr opencv/build_wasm/opencv ./
+#     rm opencv-em.zip
+# fi
+
 if [ ! -d "./build" ]
 then
     mkdir build
@@ -20,4 +28,4 @@ else
     make
 fi
 
-mv detect_face_wasm.* ..
+mv face_tracker_wasm.* ../js/
