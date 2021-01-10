5dbfca07d539dfe1297a1a854d9ba862ec522095
Edward <lued2018@gmail.com>
2021-01-09 20:13:01 -0600

    merge with master

--- js/script.js
+++ js/script.js
@@ -294,7 +294,7 @@ ARENA.FaceTracker = (function() {
                         if (msg.features && msg.pose) {
                             const faceJSON = createFaceJSON(valid, msg.features, msg.pose);
                             if (faceJSON != prevJSON) {
-                                publish(ARENA.outputTopic + ARENA.camName + '/face', faceJSON);
+                                ARENA.mqtt.publish(ARENA.outputTopic + ARENA.camName + '/face', faceJSON);
                                 prevJSON = faceJSON;
                             }
                         }
@@ -409,7 +409,7 @@ ARENA.FaceTracker = (function() {
             overlayCanvas.style.zIndex = 9998;
             document.body.appendChild(overlayCanvas);
 
-            grayscale = new ARENAFaceTracker.GrayScaleMedia(video, width, height);
+            grayscale = new FaceTracker.GrayScaleMedia(video, width, height);
         },
 
         running: function() {
